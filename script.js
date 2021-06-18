e7e022426a3d7d51191c27d13dd8787e1e6a4cdc
HafsaWoyessa <84517071+Hafsa-Woyessa@users.noreply.github.com>
2021-06-18 15:07:03 +0000

    line 2

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.getElementById('button').addEventListener('click', checkArea)
 // this code is used when the buttons are clicked. It also connect the button to the check area function.
 
 let area = 0
-let userbase = 0;
+let userbase = 0
 let userheight = 0
 let userarea = 0
-let  answer = 0
+let answer = 0
 // The let variables are where users enter the values to tell the program what to use in order to calculate the problem and find the result or answer.
 
 function checkArea () {
@@ -15,12 +15,12 @@ function checkArea () {
   area = userbase * userheight / 2
   area = parseInt(area)
 
-// this compares the userarea to the area to check to see if its correct. If its correct the if statement will be true and will run this loop displaying the "Correct" message.
-if(userarea >= area) {
-   document.getElementById('answer').innerHTML = 'Correct!'
-}  else if (userarea !== area) {
+ // this compares the userarea to the area to check to see if its correct. If its correct the if statement will be true and will run this loop displaying the "Correct" message.
+if (userarea >= area) {
+  document.getElementById('answer').innerHTML = 'Correct!'
+}else if (userarea !== area) {
   document.getElementById('answer').innerHTML = 'Wrong'
   // the else if function will only run if the first if statment is false,if the  user area does not equal to the area.
- }
+  }
 }
 // The function result tells the code what equation to follow and gives us the result of our equation. The innerHTMl displays a message on the users screen.
