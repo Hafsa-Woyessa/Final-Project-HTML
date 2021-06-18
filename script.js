8c6e18107b741ff15faa325f6b6ff8b48022e9ef
HafsaWoyessa <84517071+Hafsa-Woyessa@users.noreply.github.com>
2021-06-18 13:45:30 +0000

    line 4

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.getElementById('button').addEventListener('click', checkArea)
-// this code is used when the buttons are clicked. It also connect the button to the check area function. 
+// this code is used when the buttons are clicked. It also connect the button to the check area function.
 
 let area = 0
-let userbase = 0
-let userheight = 0
+let const = 0
+let const = 0
 let userarea = 0
-let answer = 0
+let const = 0
 // The let variables are where users enter the values to tell the program what to use in order to calculate the problem and find the result or answer.
 
 function checkArea () {
   userarea = document.getElementById('userarea').value
-  userarea = parseInt(userarea)  
+  userarea = parseInt(userarea)
 
- area = userbase*userheight/2 
+  area = userbase * userheight / 2 
  area = parseInt(area)
 
 // this compares the userarea to the area to check to see if its correct. If its correct the if statement will be true and will run this loop displaying the "Correct" message. 
