aae54e3809e6885cfb47eeac4d8a22edbdad984b
HafsaWoyessa <84517071+Hafsa-Woyessa@users.noreply.github.com>
2021-06-18 15:28:15 +0000

    line 2

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let area = 0
 const userbase = 0
 const userheight = 0
 let userarea = 0
-const answer = 0
 // The let variables are where users enter the values to tell the program what to use in order to calculate the problem and find the result or answer.
 
 function checkArea () {
@@ -17,9 +16,9 @@ function checkArea () {
 
   // this compares the userarea to the area to check to see if its correct. If its correct the if statement will be true and will run this loop displaying the "Correct" message.
   if (userarea >= area) {
-    document.getElementById('answer').innerHTML = 'Correct!'
-}  else if (userarea !== area) {
-    document.getElementById('answer').innerHTML = 'Wrong'
+    document.getElementById('area').innerHTML = 'Correct!'
+  }  else if (userarea !== area) {
+    document.getElementById('area').innerHTML = 'Wrong'
   // the else if function will only run if the first if statment is false,if the  user area does not equal to the area.
   }
 }
